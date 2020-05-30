0a74b423d18aa685d7bc7dd6d0872a80dca255a9
francisco28 <fgallegosgo@gmail.com>
2020-05-30 11:04:56 -0600

    creating state and reading data of Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+
+
+    //state of the Form
+    const [ search, saveSearch ] = useState({
+        city: '',
+        country: ''
+    });
+
+    //extract city and country
+    const { city, country } = search;
+
+    //function that set the elements in the state
+    const handleChange = e => {
+        //update state
+        saveSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        });
+    }
+
     return ( 
 
         <form>
@@ -9,6 +29,8 @@ const Form = () => {
                     type="text"
                     name="city"
                     id="city"
+                    value={city}
+                    onChange={handleChange}
                 />
                 <label htmlFor="city">City: </label>
             </div>
@@ -17,8 +39,17 @@ const Form = () => {
                 <select
                     name="country"
                     id="country"
+                    value={country}
+                    onChange={handleChange}
                 >
                     <option value="">-- Select an country --</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
 
                 </select>
                 <label htmlFor="country">Country: </label>
