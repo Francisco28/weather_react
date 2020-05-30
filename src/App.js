3c49707809df27dae0eb0ff14f2d45c97c80814f
francisco28 <fgallegosgo@gmail.com>
2020-05-30 10:47:29 -0600

    first steps with Form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header';
+import Form from './components/Form';
+
 
 function App() {
   return (
@@ -8,6 +10,20 @@ function App() {
         <Header 
           title='Weather React App'
         />
+
+        <div className="container-form">
+            <div className="container">
+                <div className="row">
+                  <div className="col m6 s12">
+                      <Form />
+                  </div>
+                  <div className="col m6 s12">
+                      2
+                  </div>
+                </div>
+            </div>
+        </div>
+
     </Fragment>
     
   );
