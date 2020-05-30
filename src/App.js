7475e5c2c01b6949643b2eef609d72194d483697
francisco28 <fgallegosgo@gmail.com>
2020-05-30 10:26:31 -0600

    add component Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
-
+import React, { Fragment } from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Weather React</h1>
+
+    <Fragment>
+        <Header 
+          title='Weather React App'
+        />
+    </Fragment>
+    
   );
 }
 
